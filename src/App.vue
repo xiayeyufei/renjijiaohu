a161868acc1ac4ba0199b7cfe3100013ff7fa3d3
xiayeyufei <53027408+xiayeyufei@users.noreply.github.com>
2021-05-13 22:28:04 +0800

    人机交互

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>//213213
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
     </div>
     <router-view/>
   </div>
