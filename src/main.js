635da87df7e1e3caf15914f3251cee0f8d9a9618
xiayeyufei <53027408+xiayeyufei@users.noreply.github.com>
2021-05-14 11:46:43 +0800

    增加了vuetify

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import index from './index'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import './plugins/element.js'
 
 Vue.config.productionTip = false
 
